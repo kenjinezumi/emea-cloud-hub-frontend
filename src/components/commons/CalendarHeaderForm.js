a64e70690d08959f63deda8e45f7021eee152e44
kenjinezumi <kenji.tsuchiya@ovo.com>
2024-01-04 14:25:22 +0100

    Add calendarheaderform

--- src/components/commons/CalendarHeaderForm.js
+++ src/components/commons/CalendarHeaderForm.js
@@ -2,6 +2,10 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../../assets/svg/logo.svg";
 import beta from "../../assets/svg/beta.svg";
+import { ReactComponent as LocationLogo } from "../../assets/svg/location.svg";
+import { ReactComponent as InformationLogo } from "../../assets/svg/information.svg";
+import {ReactComponent as UsersLogo} from '../../assets/svg/users.svg';
+import {ReactComponent as LinksLogo} from '../../assets/svg/links.svg';
 
 const NavigationSidebar = () => {
   const navigate = useNavigate();
@@ -17,37 +21,72 @@ const NavigationSidebar = () => {
 
   return (
     <div className="fixed right-0 top-16 h-[calc(100vh-64px)] w-60 bg-white shadow-md p-4">
-      {" "}
+      <div className="text-lg font-semibold mb-2">Sections</div>{" "}
       <button
         onClick={() => navigateTo("/create-event")}
-        className="block w-full text-left p-2 hover:bg-gray-200 rounded"
+        className="block w-full text-left p-2 hover:bg-gray-200 rounded flex items-center"
       >
-        About
+        <span className="mr-2">
+          <svg
+            width="20px"
+            height="20px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              opacity="0.15"
+              d="M4 19C4 20.1046 4.89543 21 6 21H18C19.1046 21 20 20.1046 20 19V11H4V19Z"
+              fill="#001A72"
+            />
+            <path
+              d="M15 3V7M9 3V7M4 11H20M20 11V19C20 20.1046 19.1046 21 18 21H6C4.89543 21 4 20.1046 4 19V7C4 5.89543 4.89543 5 6 5H18C19.1046 5 20 5.89543 20 7V11Z"
+              stroke="#001A72"
+            />
+          </svg>
+        </span>{" "}
+        <span>About</span>
       </button>
       <button
         onClick={() => navigateTo("/location")}
-        className="block w-full text-left p-2 hover:bg-gray-200 rounded"
+        className="block w-full text-left p-2 hover:bg-gray-200 rounded flex items-center"
       >
-        Location
+        <span className="mr-2">
+          <LocationLogo style={{ width: "20px", height: "20px" }} />
+        </span>
+        <span>Location</span>
       </button>
       <button
         onClick={() => navigateTo("/extra")}
-        className="block w-full text-left p-2 hover:bg-gray-200 rounded"
+        className="block w-full text-left p-2 hover:bg-gray-200 rounded flex items-center"
       >
-        Extra
+        <span className="mr-2">
+        <InformationLogo style={{ width: "20px", height: "20px" }} />
+        </span>
+        <span>Extra details</span>
       </button>
+
       <button
         onClick={() => navigateTo("/audience")}
-        className="block w-full text-left p-2 hover:bg-gray-200 rounded"
+        className="block w-full text-left p-2 hover:bg-gray-200 rounded flex items-center"
       >
-        Audience
+        <span className="mr-2">
+        <UsersLogo style={{ width:'20px', height:'20px' }} />
+        </span>
+        <span>Audience</span>
       </button>
+
       <button
         onClick={() => navigateTo("/links")}
-        className="block w-full text-left p-2 hover:bg-gray-200 rounded"
+        className="block w-full text-left p-2 hover:bg-gray-200 rounded flex items-center"
       >
-        Links
+        <span className="mr-2">
+        <LinksLogo style={{  width:'20px', height:'20px' }} />
+        </span>
+        <span>Links</span>
       </button>
+
+     
       <hr className="my-4" />
       <button
         onClick={handleCancel}
@@ -62,8 +101,6 @@ const NavigationSidebar = () => {
 export default function CalendarHeaderForm() {
   const navigate = useNavigate();
 
- 
-
   return (
     <>
       <NavigationSidebar />
@@ -75,7 +112,6 @@ export default function CalendarHeaderForm() {
           </h1>
           <img src={beta} alt="beta" className="mr-2 w-12 h-12" />
         </div>
-       
       </header>
     </>
   );
