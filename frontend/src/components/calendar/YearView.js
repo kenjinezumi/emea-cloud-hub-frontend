2779b3f318f0195125d462e25c52715138ed2210
kenjinezumi <kenji.tsuchiya@ovo.com>
2024-02-15 01:46:28 +0000

    Fix different padding for yearview

--- frontend/src/components/calendar/YearView.js
+++ frontend/src/components/calendar/YearView.js
@@ -1,22 +1,22 @@
-import React, {useContext, useEffect, useState} from 'react';
-import dayjs from 'dayjs';
-import MonthView from './MonthView';
-import {createYearData} from '../../util';
-import GlobalContext from '../../context/GlobalContext';
-import {Grid, Typography, Paper, Box, Chip, Tooltip} from '@mui/material'; // Import Box for styling
-import '../styles/Yearview.css';
-import {useLocation} from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";
+import dayjs from "dayjs";
+import MonthView from "./MonthView";
+import { createYearData } from "../../util";
+import GlobalContext from "../../context/GlobalContext";
+import { Grid, Typography, Paper, Box, Chip, Tooltip } from "@mui/material"; 
+import "../styles/Yearview.css";
+import { useLocation } from "react-router-dom";
 
-import {getDummyEventData} from '../../api/getDummyData'; // Assuming this is your API call
+import { getDummyEventData } from "../../api/getDummyData"; 
 
-import CameraIndoorIcon from '@mui/icons-material/CameraIndoor';
-import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
-import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
-import CampaignIcon from '@mui/icons-material/Campaign';
-import LaptopIcon from '@mui/icons-material/Laptop';
+import CameraIndoorIcon from "@mui/icons-material/CameraIndoor";
+import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
+import EmojiPeopleIcon from "@mui/icons-material/EmojiPeople";
+import CampaignIcon from "@mui/icons-material/Campaign";
+import LaptopIcon from "@mui/icons-material/Laptop";
 
 export default function YearView() {
-  const {daySelected, setShowEventModal, setDaySelected} =
+  const { daySelected, setShowEventModal, setDaySelected } =
     useContext(GlobalContext);
   const [events, setEvents] = useState([]); // Use useState to initialize events as an empty array
 
@@ -33,16 +33,16 @@ export default function YearView() {
       const eventData = await getDummyEventData();
       setEvents(eventData);
     } catch (error) {
-      console.error('Error fetching event data:', error);
+      console.error("Error fetching event data:", error);
     }
   };
 
   const eventTypeCounts = {
-    'Online Event': Array(12).fill(0), // Initialize counts for each month with 0
-    'Blog Post': Array(12).fill(0),
-    'Customer Story': Array(12).fill(0),
-    'Hybrid Event': Array(12).fill(0),
-    'Physical Event': Array(12).fill(0),
+    "Online Event": Array(12).fill(0), // Initialize counts for each month with 0
+    "Blog Post": Array(12).fill(0),
+    "Customer Story": Array(12).fill(0),
+    "Hybrid Event": Array(12).fill(0),
+    "Physical Event": Array(12).fill(0),
   };
 
   useEffect(() => {
@@ -67,13 +67,13 @@ export default function YearView() {
 
   return (
     <div
-      style={{padding: 16, marginLeft: '40px', width: '90%', align: 'center'}}
+      style={{ padding: 16, marginLeft: "40px", width: "90%", align: "center" }}
     >
       <Typography
         variant="h6"
         align="center"
         gutterBottom
-        style={{marginBottom: '20px'}}
+        style={{ marginBottom: "20px" }}
       >
         Year Overview - {year}
       </Typography>
@@ -81,18 +81,18 @@ export default function YearView() {
         {yearData.map((month, index) => (
           <Grid key={index} item xs={12} sm={6} md={4}>
             <Paper className="month-container">
-              {' '}
+              {" "}
               {/* Add styling using Paper */}
               <div
                 onClick={() => handleAddEvent(index)}
                 style={{
-                  cursor: 'pointer',
-                  padding: '16px',
-                  border: '1px solid transparent',
+                  cursor: "pointer",
+                  padding: "16px",
+                  border: "1px solid transparent",
                 }}
               >
-                <Typography align="center" style={{marginBottom: '4px'}}>
-                  {dayjs(new Date(year, index)).format('MMMM')}
+                <Typography align="center" style={{ marginBottom: "4px" }}>
+                  {dayjs(new Date(year, index)).format("MMMM")}
                 </Typography>
                 <Box
                   display="flex"
@@ -101,65 +101,55 @@ export default function YearView() {
                   flexWrap="wrap"
                   gap={2}
                 >
-                  <Tooltip title='Online event'>
+                  <Tooltip title="Online event">
                     <Chip
                       icon={<CameraIndoorIcon />}
-                      title="Online event"
-                      label={eventTypeCounts['Online Event'][index]}
+                      label={eventTypeCounts["Online Event"][index]}
                       variant="outlined"
                       size="small"
-                      style={{margin: '4px'}}
+                      style={{ margin: "4px" }}
                     />
                   </Tooltip>
-                  <Tooltip title='Blog post'>
-
+                  <Tooltip title="Blog post">
                     <Chip
                       icon={<LaptopIcon />}
-                      title="Blog post"
-                      label={eventTypeCounts['Blog Post'][index]}
+                      label={eventTypeCounts["Blog Post"][index]}
                       variant="outlined"
                       size="small"
-                      style={{margin: '4px'}}
+                      style={{ margin: "4px" }}
                     />
                   </Tooltip>
 
-                  <Tooltip title='Customer story'>
-
+                  <Tooltip title="Customer story">
                     <Chip
                       icon={<CampaignIcon />}
-                      title="Customer story"
-                      label={eventTypeCounts['Customer Story'][index]}
+                      label={eventTypeCounts["Customer Story"][index]}
                       variant="outlined"
                       size="small"
-                      style={{margin: '4px'}}
+                      style={{ margin: "4px" }}
                     />
                   </Tooltip>
 
-                  <Tooltip title='Hybrid event'>
-
+                  <Tooltip title="Hybrid event">
                     <Chip
                       icon={<EmojiPeopleIcon />}
-                      title="Hybrid event"
-                      label={eventTypeCounts['Hybrid Event'][index]}
+                      label={eventTypeCounts["Hybrid Event"][index]}
                       variant="outlined"
                       size="small"
-                      style={{margin: '4px'}}
+                      style={{ margin: "4px" }}
                     />
                   </Tooltip>
-                  <Tooltip title='Physical event'>
-
+                  <Tooltip title="Physical event">
                     <Chip
                       icon={<MeetingRoomIcon />}
-                      title="Physical event"
-                      label={eventTypeCounts['Physical Event'][index]}
+                      label={eventTypeCounts["Physical Event"][index]}
                       variant="outlined"
                       size="small"
-                      style={{margin: '4px'}}
+                      style={{ margin: "4px" }}
                     />
                   </Tooltip>
-
                 </Box>
-                <div style={{padding: '8px'}}>
+                <div style={{ padding: "8px" }}>
                   <MonthView
                     month={month}
                     daySelected={daySelected}
