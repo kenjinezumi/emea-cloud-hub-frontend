e980040d5d87fc594134fb4b4d1e7c0159a60173
kenjinezumi <kenji.tsuchiya@ovo.com>
2024-02-19 14:07:36 +0000

    Add cors header

--- backend/index.js
+++ backend/index.js
@@ -70,6 +70,13 @@ app.get('/event/:eventId', async (req, res) => {
   }
 });
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://login.corp.google.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 app.post('/saveEventData', async (req, res) => {
   const formData = req.body;
