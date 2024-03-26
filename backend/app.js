898befbe713fdcd1ebd9ed065d1e5570f2dc87a2
kenjinezumi <kenji.tsuchiya@ovo.com>
2024-03-26 15:44:42 +0000

    allow cors

--- backend/app.js
+++ backend/app.js
@@ -25,12 +25,6 @@ function loadQueries() {
   }
 }
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://cloudhub.googleplex.com, https://login.corp.google.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 app.post('/queryBigQuery', async (req, res) => {
   try {
