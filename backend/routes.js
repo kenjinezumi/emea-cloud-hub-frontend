60d03112e3840c86b5662b84aa1941e809ded91c
kenjinezumi <kenjitsuchiya1@gmail.com>
2024-10-30 00:22:31 +0000

    Add scope for auth for calendar

--- backend/routes.js
+++ backend/routes.js
@@ -78,6 +78,7 @@ module.exports = (firestoreStore) => {
                 'email',
                 'https://www.googleapis.com/auth/gmail.send', // Add Gmail send scope
                 'https://www.googleapis.com/auth/gmail.compose',
+                'https://www.googleapis.com/auth/calendar.events'
             ],
             accessType: 'offline', // To receive a refresh token for long-term access
             prompt: 'consent', // Force prompt to ensure refresh token is provided
