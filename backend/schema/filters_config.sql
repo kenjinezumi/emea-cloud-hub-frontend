ebf7937e423738252234f89f60b89757546389e7
kenjinezumi <kenjitsuchiya1@gmail.com>
2024-11-06 19:16:16 +0000

    Change the filters logic

---
+++ backend/schema/filters_config.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `google.com:cloudhub.data.filters_config` (
+  `eventId` STRING NOT NULL,
+  `ldap` STRING NOT NULL,
+  `filterName` STRING NOT NULL,  -- New column for the filter name
+  `config` ARRAY<STRUCT<
+    `subRegions` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
+    `gep` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
+    `accountSectors` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
+    `accountSegments` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
+    `buyerSegmentRollup` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
+    `productFamily` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
+    `industry` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
+    `partnerEvent` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
+    `draftStatus` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>
+  >>
+);
