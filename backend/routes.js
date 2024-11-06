fe73b01126bd08ec345c8af8643c847edd6d2621
kenjinezumi <kenjitsuchiya1@gmail.com>
2024-11-06 18:39:20 +0000

    Fix query

--- backend/routes.js
+++ backend/routes.js
@@ -596,28 +596,30 @@ WHERE eventId = @eventId;
         logger.info("POST /: Saving filter configuration.", { ldap });
 
         const upsertQuery = `
-    MERGE \`google.com:cloudhub.data.filters_config\` AS target
-    USING (
-        SELECT @ldap AS ldap, @filters_config AS filters_config
-    ) AS source
-    ON target.ldap = source.ldap
-    WHEN MATCHED THEN
-        UPDATE SET target.filters_config = ARRAY(
-            SELECT AS STRUCT name, config
-            FROM UNNEST(ARRAY(
-                SELECT DISTINCT AS STRUCT name, config
-                FROM UNNEST(target.filters_config)
-                UNION ALL
-                SELECT DISTINCT AS STRUCT name, config
-                FROM UNNEST(source.filters_config)
-            ))
-        )
-    WHEN NOT MATCHED THEN
-        INSERT (ldap, filters_config)
-        VALUES (source.ldap, source.filters_config);
-`;
-
-
+        MERGE \`google.com:cloudhub.data.filters_config\` AS target
+        USING (
+            SELECT 
+                @ldap AS ldap, 
+                ARRAY(
+                    SELECT AS STRUCT name, config
+                    FROM UNNEST(ARRAY(
+                        SELECT DISTINCT AS STRUCT name, config
+                        FROM UNNEST(target.filters_config)
+                        UNION ALL
+                        SELECT DISTINCT AS STRUCT name, config
+                        FROM UNNEST(@filters_config)
+                    ))
+                ) AS filters_config
+        ) AS source
+        ON target.ldap = source.ldap
+        WHEN MATCHED THEN
+            UPDATE SET 
+                target.filters_config = source.filters_config
+        WHEN NOT MATCHED THEN
+            INSERT (ldap, filters_config)
+            VALUES (source.ldap, source.filters_config);
+    `;
+    
 
         const options = {
           query: upsertQuery,
