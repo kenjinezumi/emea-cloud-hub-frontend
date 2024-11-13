1cfa7d4dbbde9ff87ac7363649af985bd5b9d7b3
kenjinezumi <kenjitsuchiya1@gmail.com>
2024-11-13 10:04:24 +0000

    Add all countries

--- frontend/src/components/filters/FiltersData.js
+++ frontend/src/components/filters/FiltersData.js
@@ -5,287 +5,235 @@ export const eventTypeOptions = [
 ];
 
 export const regionFilters = [
-  {label: 'APAC', checked: true},
-  {label: 'EMEA', checked: true},
-  {label: 'GLOBAL', checked: true},
-  {label: 'JAPAN', checked: true},
-  {label: 'LATAM', checked: true},
-  {label: 'NORTHAM', checked: true},
+  { label: 'EMEA', checked: true },
+  { label: 'GLOBAL', checked: true },
+  { label: 'LATAM', checked: true },
+  { label: 'NORTHAM', checked: true },
+  { label: 'JAPAC', checked: true },
 ];
 
 
 export const regionOptions = [
   'GLOBAL',
-  'APAC',
   'EMEA',
-  'JAPAN',
   'LATAM',
   'NORTHAM',
+  'JAPAC',
 ];
 
 export const regionsData = [
   {
     region: 'GLOBAL',
-    subregions: ['Alps', 'Benelux', 'CEE', 'France', 'Germany' , 'Iberia', 'Israel', 'Italy', 'MEA', 'Nordics', 'UK/IE'],
+    subregions: ['Alps', 'Benelux', 'CEE', 'France', 'Germany', 'Iberia', 'Israel', 'Italy', 'MEA', 'Nordics', 'UK/IE'],
   },
   {
     region: 'EMEA',
-    subregions: ['Alps', 'Benelux', 'CEE', 'France', 'Germany' , 'Iberia', 'Israel', 'Italy', 'MEA', 'Nordics', 'UK/IE'],
+    subregions: ['Alps', 'Benelux', 'CEE', 'France', 'Germany', 'Iberia', 'Israel', 'Italy', 'MEA', 'Nordics', 'UK/IE'],
   },
-  // Add more regions with their respective subregions here
+  {
+    region: 'JAPAC',
+    subregions: ['AuNZ', 'Greater China', 'India', 'Japan', 'Korea', 'SEA'],
+  },
+  {
+    region: 'LATAM',
+    subregions: ['Brazil', 'MCO', 'Mexico'],
+  },
 ];
 
 export const subregionsData = [
-  {
-    subregion: 'Alps',
-    countries: ['AT', 'CH'],
-  },
-  {
-    subregion: 'Benelux',
-    countries: ['BE', 'LU', 'NL'],
-  },
-  {
-    subregion: 'CEE',
-    countries: [
-      'BG', 'CY', 'CZ', 'EE', 'GR', 'HU', 'LT', 'LV', 'PL', 
-      'RO', 'RU', 'SK', 'UA'
-    ],
-  },
-  {
-    subregion: 'France',
-    countries: ['FR'],
-  },
-  {
-    subregion: 'Germany',
-    countries: ['DE'],
-  },
-  {
-    subregion: 'Iberia',
-    countries: ['ES', 'PT'],
-  },
-  {
-    subregion: 'Israel',
-    countries: ['IL'],
-  },
-  {
-    subregion: 'Italy',
-    countries: ['IT'],
-  },
-  {
-    subregion: 'MEA',
-    countries: ['AE', 'EG', 'KE', 'KW', 'NG', 'OM', 'QA', 'SA', 'TR', 'ZA'],
-  },
-  {
-    subregion: 'Nordics',
-    countries: ['DK', 'FI', 'NO', 'SE'],
-  },
-  {
-    subregion: 'UK/IE',
-    countries: ['GB', 'IE'],
-  },
+  { subregion: 'Alps', countries: ['AT', 'CH'] },
+  { subregion: 'Benelux', countries: ['BE', 'LU', 'NL'] },
+  { subregion: 'CEE', countries: ['BG', 'CY', 'CZ', 'EE', 'GR', 'HU', 'LT', 'LV', 'PL', 'RO', 'RU', 'SK', 'UA'] },
+  { subregion: 'France', countries: ['FR'] },
+  { subregion: 'Germany', countries: ['DE'] },
+  { subregion: 'Iberia', countries: ['ES', 'PT'] },
+  { subregion: 'Israel', countries: ['IL'] },
+  { subregion: 'Italy', countries: ['IT'] },
+  { subregion: 'MEA', countries: ['AE', 'EG', 'KE', 'KW', 'NG', 'OM', 'QA', 'SA', 'TR', 'ZA'] },
+  { subregion: 'Nordics', countries: ['DK', 'FI', 'NO', 'SE'] },
+  { subregion: 'UK/IE', countries: ['GB', 'IE'] },
+
+  // JAPAC Subregions
+  { subregion: 'AuNZ', countries: ['AS', 'AU', 'CX', 'CC', 'CK', 'FJ', 'PF', 'TF', 'GU', 'HM', 'KI', 'NR', 'NC', 'NZ', 'NU', 'NF', 'MP', 'PN', 'WS', 'SB', 'TK', 'TO', 'TV', 'VU'] },
+  { subregion: 'Greater China', countries: ['CN', 'HK', 'MO', 'MN', 'TW'] },
+  { subregion: 'India', countries: ['IN', 'MH', 'PK', 'LK'] },
+  { subregion: 'Japan', countries: ['JP'] },
+  { subregion: 'Korea', countries: ['KP', 'KR'] },
+  { subregion: 'SEA', countries: ['BD', 'BT', 'BN', 'KH', 'ID', 'LA', 'MY', 'MV', 'FM', 'MM', 'NP', 'PW', 'PG', 'PH', 'SG', 'TH', 'TL', 'VN'] },
+
+  // LATAM Subregions
+  { subregion: 'Brazil', countries: ['BR'] },
+  { subregion: 'MCO', countries: ['AR', 'AI', 'AG', 'AW', 'BS', 'BB', 'BZ', 'BM', 'BO', 'BQ', 'KY', 'CR', 'CW', 'DM', 'DO', 'EC', 'SV', 'GD', 'GP', 'GT', 'GY', 'HT', 'HN', 'JM', 'MQ', 'NI', 'PA', 'KN', 'LC', 'MF', 'PM', 'VC', 'SR', 'TT', 'TC', 'VE', 'VG', 'VI', 'CU', 'FK', 'GF', 'MS', 'SX', 'GS', 'UM', 'PE', 'PY', 'UY', 'CL', 'CO'] },
+  { subregion: 'Mexico', countries: ['MX'] },
 ];
+
 export const countriesData = [
-  {
-    country: 'AT',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'Alps',
-  },
-  {
-    country: 'CH',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'Alps',
-  },
-  {
-    country: 'BE',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'Benelux',
-  },
-  {
-    country: 'LU',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'Benelux',
-  },
-  {
-    country: 'NL',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'Benelux',
-  },
-  {
-    country: 'BG',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'CEE',
-  },
-  {
-    country: 'CY',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'CEE',
-  },
-  {
-    country: 'CZ',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'CEE',
-  },
-  {
-    country: 'EE',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'CEE',
-  },
-  {
-    country: 'GR',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'CEE',
-  },
-  {
-    country: 'HU',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'CEE',
-  },
-  {
-    country: 'LT',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'CEE',
-  },
-  {
-    country: 'LV',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'CEE',
-  },
-  {
-    country: 'PL',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'CEE',
-  },
-  {
-    country: 'RO',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'CEE',
-  },
-  {
-    country: 'RU',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'CEE',
-  },
-  {
-    country: 'SK',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'CEE',
-  },
-  {
-    country: 'UA',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'CEE',
-  },
-  {
-    country: 'FR',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'France',
-  },
-  {
-    country: 'DE',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'Germany',
-  },
-  {
-    country: 'IT',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'Italy',
-  },
-  {
-    country: 'ES',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'Iberia',
-  },
-  {
-    country: 'PT',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'Iberia',
-  },
-  {
-    country: 'IL',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'Israel',
-  },
-  {
-    country: 'AE',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'MEA',
-  },
-  {
-    country: 'EG',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'MEA',
-  },
-  {
-    country: 'KE',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'MEA',
-  },
-  {
-    country: 'KW',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'MEA',
-  },
-  {
-    country: 'NG',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'MEA',
-  },
-  {
-    country: 'OM',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'MEA',
-  },
-  {
-    country: 'QA',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'MEA',
-  },
-  {
-    country: 'SA',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'MEA',
-  },
-  {
-    country: 'TR',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'MEA',
-  },
-  {
-    country: 'ZA',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'MEA',
-  },
-  {
-    country: 'DK',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'Nordics',
-  },
-  {
-    country: 'FI',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'Nordics',
-  },
-  {
-    country: 'NO',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'Nordics',
-  },
-  {
-    country: 'SE',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'Nordics',
-  },
-  {
-    country: 'GB',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'UK/IE',
-  },
-  {
-    country: 'IE',
-    region: ['EMEA', 'GLOBAL'],
-    subregion: 'UK/IE',
-  },
+  // EMEA
+  { country: 'AT', region: ['EMEA', 'GLOBAL'], subregion: 'Alps' },
+  { country: 'CH', region: ['EMEA', 'GLOBAL'], subregion: 'Alps' },
+  { country: 'BE', region: ['EMEA', 'GLOBAL'], subregion: 'Benelux' },
+  { country: 'LU', region: ['EMEA', 'GLOBAL'], subregion: 'Benelux' },
+  { country: 'NL', region: ['EMEA', 'GLOBAL'], subregion: 'Benelux' },
+  { country: 'BG', region: ['EMEA', 'GLOBAL'], subregion: 'CEE' },
+  { country: 'CY', region: ['EMEA', 'GLOBAL'], subregion: 'CEE' },
+  { country: 'CZ', region: ['EMEA', 'GLOBAL'], subregion: 'CEE' },
+  { country: 'EE', region: ['EMEA', 'GLOBAL'], subregion: 'CEE' },
+  { country: 'GR', region: ['EMEA', 'GLOBAL'], subregion: 'CEE' },
+  { country: 'HU', region: ['EMEA', 'GLOBAL'], subregion: 'CEE' },
+  { country: 'LT', region: ['EMEA', 'GLOBAL'], subregion: 'CEE' },
+  { country: 'LV', region: ['EMEA', 'GLOBAL'], subregion: 'CEE' },
+  { country: 'PL', region: ['EMEA', 'GLOBAL'], subregion: 'CEE' },
+  { country: 'RO', region: ['EMEA', 'GLOBAL'], subregion: 'CEE' },
+  { country: 'RU', region: ['EMEA', 'GLOBAL'], subregion: 'CEE' },
+  { country: 'SK', region: ['EMEA', 'GLOBAL'], subregion: 'CEE' },
+  { country: 'UA', region: ['EMEA', 'GLOBAL'], subregion: 'CEE' },
+  { country: 'FR', region: ['EMEA', 'GLOBAL'], subregion: 'France' },
+  { country: 'DE', region: ['EMEA', 'GLOBAL'], subregion: 'Germany' },
+  { country: 'IT', region: ['EMEA', 'GLOBAL'], subregion: 'Italy' },
+  { country: 'ES', region: ['EMEA', 'GLOBAL'], subregion: 'Iberia' },
+  { country: 'PT', region: ['EMEA', 'GLOBAL'], subregion: 'Iberia' },
+  { country: 'IL', region: ['EMEA', 'GLOBAL'], subregion: 'Israel' },
+  { country: 'AE', region: ['EMEA', 'GLOBAL'], subregion: 'MEA' },
+  { country: 'EG', region: ['EMEA', 'GLOBAL'], subregion: 'MEA' },
+  { country: 'KE', region: ['EMEA', 'GLOBAL'], subregion: 'MEA' },
+  { country: 'KW', region: ['EMEA', 'GLOBAL'], subregion: 'MEA' },
+  { country: 'NG', region: ['EMEA', 'GLOBAL'], subregion: 'MEA' },
+  { country: 'OM', region: ['EMEA', 'GLOBAL'], subregion: 'MEA' },
+  { country: 'QA', region: ['EMEA', 'GLOBAL'], subregion: 'MEA' },
+  { country: 'SA', region: ['EMEA', 'GLOBAL'], subregion: 'MEA' },
+  { country: 'TR', region: ['EMEA', 'GLOBAL'], subregion: 'MEA' },
+  { country: 'ZA', region: ['EMEA', 'GLOBAL'], subregion: 'MEA' },
+  { country: 'DK', region: ['EMEA', 'GLOBAL'], subregion: 'Nordics' },
+  { country: 'FI', region: ['EMEA', 'GLOBAL'], subregion: 'Nordics' },
+  { country: 'NO', region: ['EMEA', 'GLOBAL'], subregion: 'Nordics' },
+  { country: 'SE', region: ['EMEA', 'GLOBAL'], subregion: 'Nordics' },
+  { country: 'GB', region: ['EMEA', 'GLOBAL'], subregion: 'UK/IE' },
+  { country: 'IE', region: ['EMEA', 'GLOBAL'], subregion: 'UK/IE' },
+
+  // JAPAC - AuNZ
+  { country: 'AS', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'AU', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'CX', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'CC', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'CK', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'FJ', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'PF', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'TF', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'GU', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'HM', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'KI', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'NR', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'NC', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'NZ', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'NU', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'NF', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'MP', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'PN', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'WS', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'SB', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'TK', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'TO', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'TV', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+  { country: 'VU', region: ['JAPAC', 'GLOBAL'], subregion: 'AuNZ' },
+
+  // JAPAC - Greater China
+  { country: 'CN', region: ['JAPAC', 'GLOBAL'], subregion: 'Greater China' },
+  { country: 'HK', region: ['JAPAC', 'GLOBAL'], subregion: 'Greater China' },
+  { country: 'MO', region: ['JAPAC', 'GLOBAL'], subregion: 'Greater China' },
+  { country: 'MN', region: ['JAPAC', 'GLOBAL'], subregion: 'Greater China' },
+  { country: 'TW', region: ['JAPAC', 'GLOBAL'], subregion: 'Greater China' },
+
+  // JAPAC - India
+  { country: 'IN', region: ['JAPAC', 'GLOBAL'], subregion: 'India' },
+  { country: 'MH', region: ['JAPAC', 'GLOBAL'], subregion: 'India' },
+  { country: 'PK', region: ['JAPAC', 'GLOBAL'], subregion: 'India' },
+  { country: 'LK', region: ['JAPAC', 'GLOBAL'], subregion: 'India' },
+
+  // JAPAC - Japan
+  { country: 'JP', region: ['JAPAC', 'GLOBAL'], subregion: 'Japan' },
+
+  // JAPAC - Korea
+  { country: 'KP', region: ['JAPAC', 'GLOBAL'], subregion: 'Korea' },
+  { country: 'KR', region: ['JAPAC', 'GLOBAL'], subregion: 'Korea' },
+
+  // JAPAC - SEA
+  { country: 'BD', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'BT', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'BN', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'KH', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'ID', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'LA', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'MY', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'MV', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'FM', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'MM', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'NP', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'PW', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'PG', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'PH', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'SG', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'TH', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'TL', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+  { country: 'VN', region: ['JAPAC', 'GLOBAL'], subregion: 'SEA' },
+
+  // LATAM - Brazil
+  { country: 'BR', region: ['LATAM', 'GLOBAL'], subregion: 'Brazil' },
+
+  // LATAM - MCO
+  { country: 'AR', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'AI', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'AG', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'AW', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'BS', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'BB', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'BZ', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'BM', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'BO', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'BQ', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'KY', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'CR', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'CW', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'DM', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'DO', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'EC', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'SV', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'GD', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'GP', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'GT', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'GY', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'HT', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'HN', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'JM', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'MQ', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'NI', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'PA', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'KN', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'LC', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'MF', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'PM', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'VC', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'SR', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'TT', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'TC', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'VE', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'VG', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'VI', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'CU', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'FK', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'GF', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'MS', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'SX', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'GS', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'UM', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'PE', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'PY', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'UY', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'CL', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  { country: 'CO', region: ['LATAM', 'GLOBAL'], subregion: 'MCO' },
+  // LATAM - Mexico
+  { country: 'MX', region: ['LATAM', 'GLOBAL'], subregion: 'Mexico' },
 ];
+
 
 
 export const subRegionOptions = [
@@ -300,15 +248,60 @@ export const subRegionOptions = [
   'MEA',
   'Nordics',
   'UK/IE',
+  'AuNZ',
+  'Greater China',
+  'India',
+  'Japan',
+  'Korea',
+  'SEA',
+  'Brazil',
+  'MCO',
+  'Mexico',
 ];
+
 
 
 export const countryOptions = [
+  // EMEA
   'AT', 'CH', 'BE', 'LU', 'NL', 'BG', 'CY', 'CZ', 'EE', 'GR', 'HU', 
   'LT', 'LV', 'PL', 'RO', 'RU', 'SK', 'UA', 'FR', 'DE', 'IT', 'ES', 
   'PT', 'IL', 'AE', 'EG', 'KE', 'KW', 'NG', 'OM', 'QA', 'SA', 'TR', 
-  'ZA', 'DK', 'FI', 'NO', 'SE', 'GB', 'IE'
+  'ZA', 'DK', 'FI', 'NO', 'SE', 'GB', 'IE',
+  
+  // JAPAC - AuNZ
+  'AS', 'AU', 'CX', 'CC', 'CK', 'FJ', 'PF', 'TF', 'GU', 'HM', 'KI', 'NR', 
+  'NC', 'NZ', 'NU', 'NF', 'MP', 'PN', 'WS', 'SB', 'TK', 'TO', 'TV', 'VU',
+
+  // JAPAC - Greater China
+  'CN', 'HK', 'MO', 'MN', 'TW',
+
+  // JAPAC - India
+  'IN', 'MH', 'PK', 'LK',
+
+  // JAPAC - Japan
+  'JP',
+
+  // JAPAC - Korea
+  'KP', 'KR',
+
+  // JAPAC - SEA
+  'BD', 'BT', 'BN', 'KH', 'ID', 'LA', 'MY', 'MV', 'FM', 'MM', 'NP', 'PW', 
+  'PG', 'PH', 'SG', 'TH', 'TL', 'VN',
+
+  // LATAM - Brazil
+  'BR',
+
+  // LATAM - MCO
+  'AR', 'AI', 'AG', 'AW', 'BS', 'BB', 'BZ', 'BM', 'BO', 'BQ', 'KY', 'CR', 
+  'CW', 'DM', 'DO', 'EC', 'SV', 'GD', 'GP', 'GT', 'GY', 'HT', 'HN', 'JM', 
+  'MQ', 'NI', 'PA', 'KN', 'LC', 'MF', 'PM', 'VC', 'SR', 'TT', 'TC', 'VE', 
+  'VG', 'VI', 'CU', 'FK', 'GF', 'MS', 'SX', 'GS', 'UM', 'PE', 'PY', 'UY', 
+  'CL', 'CO',
+
+  // LATAM - Mexico
+  'MX'
 ];
+
 
 
 export const languageOptions = [
