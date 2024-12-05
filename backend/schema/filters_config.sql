dcafc1afe7dce01f22bf69f57fe044b3fc59cc62
kenjinezumi <kenjitsuchiya1@gmail.com>
2024-12-05 17:13:28 +0000

    Change to the sql for filters config

--- backend/schema/filters_config.sql
+++ backend/schema/filters_config.sql
@@ -1,17 +1,21 @@
 CREATE TABLE `google.com:cloudhub.data.filters_config` (
   `id` STRING NOT NULL,
   `ldap` STRING NOT NULL,
-  `filterName` STRING NOT NULL,  -- New column for the filter name
+  `filterName` STRING NOT NULL,
   `config` ARRAY<STRUCT<
-    `regions` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>
+    `regions` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
     `subRegions` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
     `countries` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
     `gep` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
-    `accountSectors` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
+    `programName` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
+    `accountSectors` ARRAY<STRUCT<`value` STRING, `checked` BOOL>>, 
     `accountSegments` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
     `buyerSegmentRollup` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
     `productFamily` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
     `industry` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
     `partnerEvent` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
-    `draftStatus` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,   
+    `draftStatus` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
+    `activityType` ARRAY<STRUCT<`label` STRING, `checked` BOOL>>,
+    `organisedBy` ARRAY<STRING> 
+  >>
 );
